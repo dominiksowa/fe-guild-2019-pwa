cd5e90488e7b5c993ecf10d68ed244f1fdf5aa2e
Sowa <dominik.sowa@ing.com>
2019-11-14 17:46:44 +0100

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -102,3 +102,19 @@ window.addEventListener('beforeinstallprompt', event => {
 
     return false;
 });
+
+window.addEventListener('load', () => {
+    ...
+    // After the existing code
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register(`${baseUrl}sw.js`)
+            .then( registration => {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(err => {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    }
+});
